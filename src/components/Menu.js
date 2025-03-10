1a8f308d58808ea97e888beb8a98411d15135b16
Diego Manildo <diegothiry26@gmail.com>
2025-03-10 13:08:53 -0300

    Centered sections

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Description from './Description';
 import "./Menu.css"
+import Proyects from './Proyects';
 
 class Menu extends Component {
   render() {
     return (
       <div>
         <Description />
+        <Proyects />
       </div>
     )
   }
---
+++ src/components/Proyects.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "./Proyects.css"
+
+class Proyects extends Component {
+  render() {
+    return (
+      <section className="proyects">
+        <h1 className="title glow">Hola</h1>
+      </section>
+    )
+  }
+}
+
+export default Proyects
